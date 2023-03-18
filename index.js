c5aa00c41c51074f5f4d3084cab10f23fb9e92d3
Alvin <exhibition315@yahoo.com.tw>
2023-03-18 16:15:57 +0800

    [Add] 1 Add Vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const line = require('@line/bot-sdk');
-const express = require('express');
+const line = require("@line/bot-sdk");
+const express = require("express");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const config = {
@@ -13,22 +13,25 @@ const client = new line.Client(config);
 const app = express();
 
 const handleEvent = (event) => {
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: "text", text: event.message.text };
   return client.replyMessage(event.replyToken, echo);
-}
+};
 
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
+app.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
       res.status(500).end();
     });
 });
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
